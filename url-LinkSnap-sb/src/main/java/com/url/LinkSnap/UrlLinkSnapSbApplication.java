088c37a83c333e39a1b88b30a926cac020966c6a
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-07-11 17:30:00 +0530

    Project Started

---
+++ url-LinkSnap-sb/src/main/java/com/url/LinkSnap/UrlLinkSnapSbApplication.java
@@ -0,0 +1,13 @@
+package com.url.LinkSnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlLinkSnapSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlLinkSnapSbApplication.class, args);
+	}
+
+}
