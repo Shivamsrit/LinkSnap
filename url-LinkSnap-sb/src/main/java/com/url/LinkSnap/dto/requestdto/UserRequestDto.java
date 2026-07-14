6d277261070f51d4d1e3f3f7dd7faa9df8019dbc
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-07-14 14:30:00 +0530

    feat : add UserRequestDto

--- url-LinkSnap-sb/src/main/java/com/url/LinkSnap/dto/requestdto/UserRequestDto.java
+++ url-LinkSnap-sb/src/main/java/com/url/LinkSnap/dto/requestdto/UserRequestDto.java
@@ -1,4 +1,14 @@
 package com.url.LinkSnap.dto.requestdto;
 
+import com.url.LinkSnap.enums.Role;
+import lombok.Data;
+
+
+@Data
 public class UserRequestDto {
+
+    private String name;
+    private String email;
+    private String password;
+    private Role role ;
 }
