79705917719cc2ea3b4f0f2bff4b2dd2dfb88256
Shivam Jaiswal <shivamsrit980@gmail.com>
2026-07-21 15:30:00 +0530

    chore(db): add MySQL database properties

--- url-LinkSnap-sb/src/main/java/com/url/LinkSnap/dto/responsedto/UserResponseDto.java
+++ url-LinkSnap-sb/src/main/java/com/url/LinkSnap/dto/responsedto/UserResponseDto.java
@@ -1,4 +1,16 @@
 package com.url.LinkSnap.dto.responsedto;
 
+import com.url.LinkSnap.enums.Role;
+import lombok.Data;
+
+@Data
+
 public class UserResponseDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private Role role ;
 }
+
